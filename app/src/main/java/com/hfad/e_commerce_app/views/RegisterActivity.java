0d5c73a267e867bd8cb951a8e9a02cff7ffcdec3
huy <huync21@gmail.com>
2022-04-06 21:07:41 +0700

    UI register and login

---
+++ app/src/main/java/com/hfad/e_commerce_app/views/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.hfad.e_commerce_app.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hfad.e_commerce_app.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
